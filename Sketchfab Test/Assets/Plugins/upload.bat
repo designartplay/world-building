fe86d60509a50f749995c4c9cffbee21282df3f5
designartplay <84252070+designartplay@users.noreply.github.com>
2021-06-26 10:56:56 +0800

    Simple revolving animation

--- Sketchfab Test/Assets/Plugins/upload.bat
+++ Sketchfab Test/Assets/Plugins/upload.bat
@@ -1,1 +1,1 @@
-curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/marcoangeles/Documents/GitHub/world-building/Sketchfab Test/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1752059830421422403.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/marcoangeles/Documents/GitHub/world-building/Sketchfab Test/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1767889017325486197.json
